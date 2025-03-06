f57d79720f7108e0bc94340ffd08abd0723442fa
Tasdik <mtasdikyousuf@gmail.com>
2025-03-06 23:01:36 +0600

    appending dotfiles

---
+++ config/i3/display.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Detect if the lid is closed
+LID_STATE=$(cat /proc/acpi/button/lid/LID/state | awk '{print $2}')
+
+if [ "$LID_STATE" = "closed" ]; then
+    xrandr --output eDP-1 --off --output HDMI-1 --mode 1920x1080 --primary
+else
+    xrandr --output eDP-1 --mode 1920x1080 --primary --output HDMI-1 --mode 1920x1080 --right-of eDP-1
+fi
+
